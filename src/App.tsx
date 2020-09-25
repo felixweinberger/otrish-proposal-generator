09a1d208a9d4295497267bf906453d735d9ce9de
Felix Weinberger <felix.weinberger@samsara.com>
2020-09-25 19:10:29 +0100

    root: set up prettier and eslint for auto-formatting on save

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AppContainer">
+      <Container>
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
